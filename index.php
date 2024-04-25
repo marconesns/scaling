cbabd47326f278eb65fc9b1d6fc38898c36fbf36
Marcones Nunes <marcones.ns@gmail.com>
2024-04-25 15:01:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <br/> Server Qual eh o IP: <?php echo $_SERVER['SERVER_ADDR']; ?> 
+ <br/> Server Qual eh o <sapn style=color:#dfdfdf>IP:</sapn> <?php echo $_SERVER['SERVER_ADDR']; ?> 
  </body>
 </html>
